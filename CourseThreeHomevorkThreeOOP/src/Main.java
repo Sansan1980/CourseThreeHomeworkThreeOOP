1523f49cbbcf98b022dbc3fce1e884b6093c00bf
Homework-10 <danilovsansan@gmail.com>
2024-02-23 14:00:48 +0300

    ДЗ 1

--- CourseThreeHomevorkThreeOOP/src/Main.java
+++ CourseThreeHomevorkThreeOOP/src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /* Привет! На связи домашнее задание урока 2.2. ООП.
+
+                Ниже вам даны несколько блоков кода. Ваша задача — поправить код так, чтобы он учитывал изученные принципы ООП.
+                Здесь нет сложного и простого уровня, вам необходимо поправить все блоки кода. Выполненное задание присылайте ссылкой на проект на GitHub.
+        Обязательные для выполнения блоки кода
+        1-й блок кода. Пример с велосипедом
+                */
     }
+
+    public class Bicycle {
+
+        private String modelName;
+
+        public String getModelName() {
+            return modelName;
+        }
+        public int wheelsCount;
+
+        public int getWheelsCount() {
+            return wheelsCount;
+        }
+
+        public void updateTyre() {
+            System.out.println("Меняем покрышку");
+        }
+    }
+
+
+
+    //--//--//--//--//
 }
